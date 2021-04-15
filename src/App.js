bdc9289ce40be1ed474883cceb2c5167c36dca11
Andy Castillo <andy.castillopalma@outlook.com>
2021-04-14 19:42:39 -0500

    Task App

--- src/App.js
+++ src/App.js
@@ -1,57 +1,19 @@
 import React from 'react';
 import './App.css';
+import tasks from './sample/tasks.json';
+import Tasks from './components/Tasks';
 
-/* function Helloworld(props) {
-  return (
-    <div id="hello">
-      <h3>{props.title}</h3>
-      {props.mytext}
-    </div>
-  );
-} */
-
-class Helloworld extends React.Component {
+class App extends React.Component {
 
   state = {
-    show: true
-  }
-
-  toggleShow = () => {
-    this.setState({show: !this.state.show})
+    tasks: tasks
   }
 
   render() {
-    if (this.state.show) {
-      return (
-        <div id="hello">
-          <h3>{this.props.title}</h3>
-          {this.props.mytext}
-          <button onClick={this.toggleShow}>Toggle Show</button>
-        </div>
-      )
-    } else {
-      return <h1>There are not elements <button onClick={this.toggleShow}>Toggle Show</button></h1>
-    }
-  }
-}
-
-// const App = () => <div>This is my component: <Helloworld/></div>
-
-/* class App extends React.Component {
-  render() {
-    return <div>This is my component: <Helloworld/></div>;
-  }
-} */
-
-function App() {
-  return (
-    <div>
-      This is my component: 
-      <Helloworld mytext="Andy" title="Component 1" />
-      <Helloworld mytext="Castillo" title="Component 2" />
-      <Helloworld mytext="Palma" title="Component 3" />
+    return <div>
+      <Tasks tasks={this.state.tasks}/>
     </div>
-  );
+  }
 }
 
 export default App;
